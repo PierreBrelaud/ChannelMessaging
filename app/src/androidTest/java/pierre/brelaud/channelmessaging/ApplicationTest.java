20734798f753cb284ec75866db647a6c8977431f
PierreBrelaud <PierreBrelaud@users.noreply.github.com>
2018-12-22 16:52:44 +0100

    test commit
    plop

--- app/src/androidTest/java/pierre/brelaud/channelmessaging/ApplicationTest.java
+++ app/src/androidTest/java/pierre/brelaud/channelmessaging/ApplicationTest.java
@@ -3,6 +3,7 @@ package pierre.brelaud.channelmessaging;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
+//test
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
